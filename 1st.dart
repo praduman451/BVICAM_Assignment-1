b05da90ae29b1e24ef32de996fbd7e43447e5439
Praduman singh <51272155+praduman451@users.noreply.github.com>
2020-10-04 20:42:31 +0530

    Update 1st.dart

--- 1st.dart
+++ 1st.dart
@@ -1,5 +1,59 @@
  
- 
-I don't undersatnd the question properly.
+import 'dart:io';
 
-So i thought to leave blank.
+void main() {
+  var customer = [
+    {
+      'id': 1,
+      'name': 'Somesh kumar',
+      'address': {'bhagwanpur', 'motijheel', 'kalyani'},
+      'orders': [
+        {'product_name': 'shirt', 'quantity': 5, 'price': 2050},
+        {'product_name': 'tshirt', 'quantity': 3, 'price': 1400},
+        {'product_name': 'pant', 'quantity': 2, 'price': 1200}
+      ]
+    },
+    {
+      'id': 2,
+      'name': 'Dinesh kumar',
+      'address': {'jawaharlal road', 'dubri', 'kalyani'},
+      'orders': [
+        {'product_name': 'shirt', 'quantity': 4, 'price': 1700},
+        {'product_name': 'tshirt', 'quantity': 3, 'price': 1400},
+        {'product_name': 'pant', 'quantity': 2, 'price': 1200}
+      ]
+    },
+    {
+      'id': 3,
+      'name': 'Ankush kumar',
+      'address': {'yadav nagar', 'motijheel', 'kalyani'},
+      'orders': [
+        {'product_name': 'shirt', 'quantity': 5, 'price': 2050},
+        {'product_name': 'tshirt', 'quantity': 3, 'price': 1400},
+        {'product_name': 'pant', 'quantity': 2, 'price': 1200}
+      ]
+    },
+    {
+      'id': 4,
+      'name': 'Tushar kumar',
+      'address': {'danapur', 'motijheel', 'kalyani'},
+      'orders': [
+        {'product_name': 'shirt', 'quantity': 3, 'price': 1500},
+        {'product_name': 'tshirt', 'quantity': 2, 'price': 900},
+        {'product_name': 'pant', 'quantity': 1, 'price': 600}
+      ]
+    },
+    {
+      'id': 5,
+      'name': 'Mohit kumar',
+      'address': {'patahi', 'motijheel', 'kalyani'},
+      'orders': [
+        {'product_name': 'shirt', 'quantity': 5, 'price': 2050},
+        {'product_name': 'tshirt', 'quantity': 3, 'price': 1400},
+        {'product_name': 'pant', 'quantity': 2, 'price': 1200}
+      ]
+    }
+  ];
+  for (int i = 0; i < customer.length; i++)
+    print("Details of ${i + 1} customer are follows \n ${customer[i]}\n");
+}
